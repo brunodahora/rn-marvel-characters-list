6280cb5de465d072cc02dd96776a6858cda25317
Bruno Vercelino da Hora <brunodahora@gmail.com>
2019-11-24 10:10:30 -0300

    Installing initial libraries and configuring eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['import', 'react', 'prettier', '@typescript-eslint'],
+  rules: {},
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+      'babel-module': {},
+    },
+  },
+};
